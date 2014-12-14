99e34f4704549219820ebfb7d153e6b31354591d
soulshake <aj@gandi.net>
2014-12-14 15:06:19 -0800

    maybe long lines are breaking something

--- pull.php
+++ pull.php
@@ -1,4 +1,4 @@
-`git pull`;                         // This will execute the `git pull` command on your instance
-header("Cache-Control: max-age=1"); // Lower the cache while we're here so the changes take effect faster
-echo "hello!";                      // So you can confirm the file is in the right place by browsing to the URL
+`git pull`;                         // Will execute `git pull` command
+header("Cache-Control: max-age=1"); // Lower the cache while we're here
+echo "hello!";   
 ?>
