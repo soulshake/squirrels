ce548311912ae99b7d0079f8ecf696985fd74e9a
soulshake <aj@gandi.net>
2014-12-14 14:49:35 -0800

    add sample git hook target

---
+++ pull.php
@@ -0,0 +1,4 @@
+`git pull`;                         // This will execute the `git pull` command on your instance
+header("Cache-Control: max-age=1"); // Lower the cache while we're here so the changes take effect faster
+echo "hello!";                      // So you can confirm the file is in the right place by browsing to the URL
+?>
